27f6fcb1cb3a3490a6a7238ccb0e0a7e715384f5
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-06-25 07:40:30 +0100

    Added test/unit/mysql_rep_admin/help_message.py

---
+++ test/unit/mysql_rep_admin/unit_test_run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Unit testing program for the program module.
+# This will run all the units tests for this program.
+# Will need to run this from the base directory where the module file
+#   is located at.
+
+echo ""
+echo "Unit testing..."
+test/unit/mysql_rep_admin/help_message.py
